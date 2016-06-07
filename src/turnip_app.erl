7e9d4f57af40392df9fb0c6a3f57ad463d7ec06a
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2016-06-07 18:06:41 +0100

    print a banner - copied from rabbit src

--- src/turnip_app.erl
+++ src/turnip_app.erl
@@ -4,6 +4,10 @@
 -export([start/2]).
 -export([stop/1]).
 
+%%------------------------------------------------------------------------------
+%% public
+%%------------------------------------------------------------------------------
+
 start(_Type, _Args) ->
 
     %% read connection props from config (use env?)
@@ -12,7 +16,26 @@ start(_Type, _Args) ->
 
     %% todo: validate config
 
+    print_banner(),
+
     turnip_sup:start_link(Config).
 
 stop(_State) ->
     ok.
+
+%%------------------------------------------------------------------------------
+%% private
+%%------------------------------------------------------------------------------
+
+print_banner() ->
+    {ok, Product} = application:get_key(id),
+    {ok, Version} = application:get_key(vsn),
+
+    io:format("~n  ##  ##         \\/ "
+              "~n  ##  ##        ####     ~s ~s."
+              "~n  ##########   ######"
+              "~n  ######  ##   ####"
+              "~n  ##########   ##" ++
+              "~n~n              Starting client..."
+              "~n",
+              [Product, Version]).
