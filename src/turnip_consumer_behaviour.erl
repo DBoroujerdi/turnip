53de00a710283735462c8912285ca292a7b5f65f
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2016-06-21 13:20:29 +0100

    added todo

--- src/turnip_consumer_behaviour.erl
+++ src/turnip_consumer_behaviour.erl
@@ -4,5 +4,6 @@
                                         {error, Reason :: string()}.
 
 %% todo: should acks be expressed as a return of handle??
+%% todo: should pass state map in on the handle
 
 -callback handle(Message :: binary()) -> atom().
